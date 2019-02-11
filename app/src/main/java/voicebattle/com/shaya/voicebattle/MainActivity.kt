76659e7518359c1cecf0b8c362da1a24b6c1ed99
s-haya-123 <ganbaru.wakenai@gmail.com>
2019-02-11 20:11:11 +0900

    initial commit

---
+++ app/src/main/java/voicebattle/com/shaya/voicebattle/MainActivity.kt
@@ -0,0 +1,12 @@
+package voicebattle.com.shaya.voicebattle
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
