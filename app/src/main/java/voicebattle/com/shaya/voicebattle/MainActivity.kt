500e50c3cae70eca38bbe69f5dd17c8e4d5d3bbb
s-haya-123 <ganbaru.wakenai@gmail.com>
2019-02-22 23:34:12 +0900

    #4

--- app/src/main/java/voicebattle/com/shaya/voicebattle/MainActivity.kt
+++ app/src/main/java/voicebattle/com/shaya/voicebattle/MainActivity.kt
@@ -1,8 +1,12 @@
 package voicebattle.com.shaya.voicebattle
 
+import android.Manifest
 import android.animation.ValueAnimator
+import android.content.pm.PackageManager
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.app.ActivityCompat
+import android.support.v4.content.ContextCompat
 import android.widget.Button
 import kotlinx.android.synthetic.main.battle_layout.*
 
@@ -15,7 +19,11 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.battle_layout)
 
-
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED) {
+            // RECORD_AUDIO の実行時パーミッションを要求
+            ActivityCompat.requestPermissions(this,
+                    arrayOf(Manifest.permission.RECORD_AUDIO), 1)
+        }
         calculate_start.setOnClickListener {
            ValueAnimator().apply {
                 setIntValues(0,100)
