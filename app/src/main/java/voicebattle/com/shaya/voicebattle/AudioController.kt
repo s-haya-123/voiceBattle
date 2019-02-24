ce2efd90cb113444ccd1e4fb9f8806f123fd4e44
s-haya-123 <ganbaru.wakenai@gmail.com>
2019-02-24 15:35:04 +0900

    [refactor] move init val

--- app/src/main/java/voicebattle/com/shaya/voicebattle/AudioController.kt
+++ app/src/main/java/voicebattle/com/shaya/voicebattle/AudioController.kt
@@ -9,32 +9,33 @@ import kotlin.math.max
 
 class AudioController {
     val audioRecord: AudioRecord
-    private var shortData: ShortArray
     init {
         val SAMPLING_RATE = 44100
         val bufSize = max(1 * 10,AudioRecord.getMinBufferSize(SAMPLING_RATE,
                 AudioFormat.CHANNEL_IN_MONO,
                 AudioFormat.ENCODING_PCM_16BIT))
         val oneFrameDataCount = bufSize/2
+
+
         audioRecord = AudioRecord(MediaRecorder.AudioSource.MIC,
                 SAMPLING_RATE,
                 AudioFormat.CHANNEL_IN_MONO,
                 AudioFormat.ENCODING_PCM_16BIT,
-                bufSize)
+                bufSize).apply {
+            val shortData = ShortArray(oneFrameDataCount)
+            // コールバックを指定
+            setRecordPositionUpdateListener(object : AudioRecord.OnRecordPositionUpdateListener {
+                // フレームごとの処理
+                override fun onPeriodicNotification(recorder: AudioRecord) {
+                    this@apply.read(shortData, 0, oneFrameDataCount)
+                }
 
-        shortData = ShortArray(oneFrameDataCount)
-        // コールバックを指定
-        audioRecord.setRecordPositionUpdateListener(object : AudioRecord.OnRecordPositionUpdateListener {
-            // フレームごとの処理
-            override fun onPeriodicNotification(recorder: AudioRecord) {
-                audioRecord.read(shortData, 0, oneFrameDataCount)
-            }
+                override fun onMarkerReached(recorder: AudioRecord) {
 
-            override fun onMarkerReached(recorder: AudioRecord) {
-
-            }
-        })
-        // コールバックが呼ばれる間隔を指定
-        audioRecord.positionNotificationPeriod = oneFrameDataCount
+                }
+            })
+            // コールバックが呼ばれる間隔を指定
+            positionNotificationPeriod = oneFrameDataCount
+        }
     }
 }
