ba78210c2510adcd42f70bcc2ce7744759712183
s-haya-123 <ganbaru.wakenai@gmail.com>
2019-03-07 23:04:59 +0900

    [add] calc circle trajectory

--- app/src/main/java/voicebattle/com/shaya/voicebattle/MeterSurface.kt
+++ app/src/main/java/voicebattle/com/shaya/voicebattle/MeterSurface.kt
@@ -1,11 +1,13 @@
 package voicebattle.com.shaya.voicebattle
 
 import android.content.Context
+import android.graphics.Canvas
 import android.graphics.Color
 import android.graphics.Paint
 import android.graphics.Point
 import android.view.SurfaceHolder
 import android.view.SurfaceView
+import kotlin.math.sin
 
 class MeterSurface(activity: MainActivity?) : SurfaceView(activity),SurfaceHolder.Callback{
     val point:Point?
@@ -35,10 +37,23 @@ class MeterSurface(activity: MainActivity?) : SurfaceView(activity),SurfaceHolde
                 }
                 point?.let{
                     drawCircle(it.x /2.toFloat(),it.y/2.toFloat(),10f,paint)
+                    drawCircleOnCircleTrajectory(this,it,paint,0f)
+                    drawCircleOnCircleTrajectory(this,it,paint,20f)
+                    drawCircleOnCircleTrajectory(this,it,paint,30f)
                 }
 
             }
             holder.unlockCanvasAndPost(canvas);
         }
+    }
+    private fun drawCircleOnCircleTrajectory(canvas: Canvas,point: Point,paint:Paint,percent: Float){
+        val (x,y) = calcMetorPoint(percent,300f,point.x/2.toFloat(),point.y/2.toFloat())
+        canvas.drawCircle(x.toFloat(),y.toFloat(),10f,paint)
+    }
+    private fun calcMetorPoint(percent:Float,r:Float,originX:Float,originY: Float): Pair<Double, Double> {
+        val theta = Math.PI * ( 1 - percent / 100)
+        val x = originX + r * Math.cos(theta)
+        val y = originY - r * sin(theta)
+        return Pair(x,y)
     }
 }
