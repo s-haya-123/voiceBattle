6a0c630f57ed177796823e469ed95de363784409
s-haya-123 <ganbaru.wakenai@gmail.com>
2019-03-17 18:10:21 +0900

    [add] get data from firebase

--- app/src/main/java/voicebattle/com/shaya/voicebattle/RankingFragment.kt
+++ app/src/main/java/voicebattle/com/shaya/voicebattle/RankingFragment.kt
@@ -2,13 +2,30 @@ package voicebattle.com.shaya.voicebattle
 
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.google.firebase.firestore.FirebaseFirestore
 
 class RankingFlagment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        getRankingFromFirebase()
         return inflater.inflate(R.layout.ranking,container,false)
+    }
+    private fun getRankingFromFirebase(){
+        val db = FirebaseFirestore.getInstance()
+        db.collection("ranking")
+                .get()
+                .addOnCompleteListener {task ->
+                    if(task.isSuccessful){
+                        task.getResult().forEach {
+                            Log.d("fbLog","${it.id} ${it.data.toString()}")
+                        }
+                    } else {
+                        Log.d("fbLog",task.exception.toString())
+                    }
+                }
     }
     companion object {
         fun newInstance()  = RankingFlagment()
